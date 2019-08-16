e172b879bdbe0db1e3518d9f9bcf9291798fbf89
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-08-16 10:34:58 -0500

    Add FancyClock component

---
+++ src/components/FancyClock.tsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import styles from "./FancyClock.module.scss";
+
+class FancyClock extends Component {
+  secondHand = React.createRef<HTMLDivElement>();
+  minsHand = React.createRef<HTMLDivElement>();
+  hourHand = React.createRef<HTMLDivElement>();
+  secondsInterval: number | null = null;
+
+  state = {
+    hoursDegrees: "90deg",
+    secondsDegrees: "90deg",
+    minsDegrees: "90deg",
+  };
+
+  componentDidMount() {
+    this.setDate();
+  }
+
+  componentWillUnmount() {
+  }
+
+  setDate() {
+    const date = new Date();
+  }
+
+  render() {
+    const { secondsDegrees, hoursDegrees, minsDegrees } = this.state;
+
+    return (
+      <div className={styles.clock}>
+        <div className={styles.clockFace}>
+          <div
+            className={styles.clockHand + " hour-hand"}
+          />
+          <div
+            className={styles.clockHand + " min-hand"}
+          />
+          <div
+            className={styles.clockHand + " second-hand"}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FancyClock;
