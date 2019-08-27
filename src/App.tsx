961fb1069f120fe9de6538e84e16a9ed7ee437e5
Luis Rogelio Hernández López <kurtz@EPMXGUAW0625.princeton.epam.com>
2019-08-27 14:06:32 -0500

    Remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
-import TodoList from "./components/TodoList";
-import NewTodoForm from "./components/NewTodoForm";
-import AnotherForm from "./components/AnotherForm";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <TodoList />
-      <NewTodoForm />
-      <AnotherForm />
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 export default App;
